43a2bc035f928e248f7c9772b721dda40260096f
getoarmorina3 <getoaarm1@gmail.com>
2024-01-19 15:59:45 +0100

    Update ToastShelf.js

--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,18 +1,16 @@
 import React from 'react';
 
+import { ToastContext } from '../ToastProvider';
 import Toast from '../Toast';
 import styles from './ToastShelf.module.css';
 
-function ToastShelf({ toasts, handleDismiss }) {
+function ToastShelf() {
+  const { toasts } = React.useContext(ToastContext);
   return (
     <ol className={styles.wrapper}>
       {toasts.map((toast) => (
         <li key={toast.id} className={styles.toastWrapper}>
-          <Toast
-            variant={toast.variant}
-            handleDismiss={handleDismiss}
-            id={toast.id}
-          >
+          <Toast variant={toast.variant} id={toast.id}>
             {toast.message}
           </Toast>
         </li>
