3fb2062325a72569fa7b8928aac8952176404518
getoarmorina3 <getoaarm1@gmail.com>
2024-01-19 15:58:54 +0100

    toast context proivder

---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([
+    {
+      id: crypto.randomUUID(),
+      message: 'This is a notice toast',
+      variant: 'notice',
+    },
+    {
+      id: crypto.randomUUID(),
+      message: 'This is a warning toast',
+      variant: 'warning',
+    },
+    {
+      id: crypto.randomUUID(),
+      message: 'This is a success toast',
+      variant: 'success',
+    },
+    {
+      id: crypto.randomUUID(),
+      message: 'This is an error toast',
+      variant: 'error',
+    },
+  ]);
+
+  function createToast(message, variant) {
+    const nextToasts = [
+      ...toasts,
+      {
+        id: crypto.randomUUID(),
+        message,
+        variant,
+      },
+    ];
+
+    setToasts(nextToasts);
+  }
+
+  function dismissToast(id) {
+    const nextToasts = toasts.filter((toast) => toast.id !== id);
+
+    setToasts(nextToasts);
+  }
+
+  return (
+    <ToastContext.Provider value={{ toasts, createToast, dismissToast }}>
+      {children}
+    </ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
